9869db7fa0cdb1e3f1fe3ec7777dcd7c8ffcc801
Håkon <haakonheuch@gmail.com>
2016-09-15 11:10:09 +0200

    test

--- http.go
+++ http.go
@@ -27,7 +27,7 @@ func (n romanGenerator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     i, err := strconv.Atoi(number)
 
     if err == nil {
-        fmt.Fprintf(w, "Here's your number: %s\n", to_roman(i))
+        fmt.Fprintf(w, "Here's your number as you wish: %s\n", to_roman(i))
     }
 }
 
